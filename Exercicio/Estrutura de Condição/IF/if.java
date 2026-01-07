46405c5eaf0120f4db533d1b546014fd090fe442
Giovanna Clemente <132226469+Gigiovh@users.noreply.github.com>
2026-01-06 22:24:40 -0300

    Create if.java
    chore: add exercise file

---
+++ Exercicio/Estrutura de Condição/IF/if.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+Faça um programa que leia dois valores numéricos e efetue a adição, caso o resultado seja maior que 10 apresentá-lo.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner caneta= new Scanner(System.in);
+	    
+		System.out.println(" Digite um número: ");
+		double n1= caneta.nextDouble();
+		System.out.println(" Digite um número: ");
+		double n2= caneta.nextDouble();
+		
+		double soma= n1+n2;
+		
+		if(soma>10){
+		    System.out.println(" O resultado da soma é: "+soma);
+		}else{
+		}
+		
+	}
+}
