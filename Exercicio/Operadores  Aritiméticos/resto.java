16a82662bea25dbf8b13c1d222e375c6257effbc
Giovanna Clemente <132226469+Gigiovh@users.noreply.github.com>
2026-01-06 22:19:49 -0300

    Create resto.java
    chore: add exercise file

---
+++ Exercicio/Operadores  Aritiméticos/resto.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Faça um programa que leia um número inteiro e mostre uma mensagem indicando se este número é par ou ímpar, e se é positivo ou negativo.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner ana= new Scanner(System.in);
+		System.out.println(" Digite um número: ");
+		double n1= ana.nextDouble();
+		if(n1%2==0){
+		    System.out.println(" O número é par ");
+		}else {
+		    System.out.println(" O número é ímpar ");
+		}if(n1<0){
+		    System.out.println(" O número é negativo ");
+		}else{
+		    System.out.println(" O número é positivo ");
+		}
+	}
+}
