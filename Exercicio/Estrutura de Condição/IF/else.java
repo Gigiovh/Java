3bbef6e0627d2d12f9c28046366e895199e2f4bf
Giovanna Clemente <132226469+Gigiovh@users.noreply.github.com>
2026-01-06 22:25:25 -0300

    Create else.java
    chore: add exercise file

---
+++ Exercicio/Estrutura de Condição/IF/else.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+ Faça um programa que leia um número e informe se ele é ou não divisível por 5.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner (System.in);
+System.out.println("coloque qualquer número");
+		double n1 = scanner.nextDouble();
+
+		
+if (n1 %5== 0)
+{
+System.out.println("o número é divisivel por 5");
+}
+else 
+System.out.println("o número não é divisivel por 5");
+
+
+
+
+
+
+
+
+}
+}
+
+
