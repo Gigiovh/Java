c0c249cd1fdd6904661d220044eb30c0a8753dac
Giovanna Clemente <132226469+Gigiovh@users.noreply.github.com>
2026-01-08 10:47:25 -0300

    Create crescente.java
    chore: add exercise file

---
+++ Exercicio/Operadores Comparativos/crescente.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+       
+20)	 Faça um programa que leia dois números e imprima-os em ordem crescente (suponha números diferentes).
+
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+public static void main (String[]args){
+Scanner run= new Scanner(System.in);
+
+System.out.println(" Digite um número: ");
+double n1= run.nextDouble();
+
+System.out.println(" Digite outro número: ");
+double n2= run.nextDouble();
+
+if(n1>n2){
+    System.out.println(" A ordem crescente dos números é: "+n1 +n2);
+}else if (n1<n2){
+    System.out.println(" A ordem crescente dos números é: "+n2 +n1);
+}else{
+    System.out.println("INVÁLIDO");
+}
+}
+}
