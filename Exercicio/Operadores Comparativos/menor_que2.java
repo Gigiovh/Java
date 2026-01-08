9c2f8c36cb7b942bf8415e361f1d3f9966ae9950
Giovanna Clemente <132226469+Gigiovh@users.noreply.github.com>
2026-01-08 10:49:21 -0300

    Update menor_que2.java
    chore: add exercise file

--- Exercicio/Operadores Comparativos/menor_que2.java
+++ Exercicio/Operadores Comparativos/menor_que2.java
@@ -7,11 +7,11 @@ import java.util.Scanner;
 public class Main
 {
 public static void main(String[]args){
-Scanner relogio=new Scanner(System.in);
+Scanner entrada=new Scanner(System.in);
 System.out.println(" Digite um número ");
-double n1=relogio.nextDouble();
+double n1=entrada.nextDouble();
 System.out.println(" Digite um número ");
-double n2=relogio.nextDouble();
+double n2=entrada.nextDouble();
 if(n1>n2){
 System.out.println(" O menor número é: "+n2);
 }else{
